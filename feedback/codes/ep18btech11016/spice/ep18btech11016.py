c3bece2d75c9773ca88d67daeb7093bb8b888f2f
Sanskar123 <sanskarnanegaonkar@gmail.com>
2020-05-25 14:49:29 +0530

    Add files via upload

--- feedback/codes/ep18btech11016/spice/ep18btech11016.py
+++ feedback/codes/ep18btech11016/spice/ep18btech11016.py
@@ -6,46 +6,32 @@ import subprocess
 import shlex
 #end if
 
-data1 = np.loadtxt('ep18btech11016_ol_gain.dat')  
+data1 = np.loadtxt('ep18btech11016_feed_gain.dat')  
 plt.plot(data1[:,0],data1[:,1])  
 plt.grid()
 plt.xlabel("Time")
 plt.ylabel("Gain")
-plt.title('Open loop gain')
+plt.title("Feedback gain")
 
 # if using termux
-plt.savefig('./figs/ep18btech11016/ep18btech11016_ol_gain.pdf')
-plt.savefig('./figs/ep18btech11016/ep18btech11016_ol_gain.eps')
-subprocess.run(shlex.split("termux-open ./figs/ep18btech11016/ep18btech11016_ol_gain.pdf"))
+#plt.savefig('./figs/ep18btech11016/ep18btech11016_feed_gain.pdf')
+#plt.savefig('./figs/ep18btech11016/ep18btech11016_feed_gain.eps')
+#subprocess.run(shlex.split("termux-open ./figs/ep18btech11016/ep18btech11016_feed_gain.pdf"))
 # else
-#plt.show()
+plt.show()
 
-data2 = np.loadtxt('ep18btech11016_feed_gain.dat')  
+data2 = np.loadtxt('ep18btech11016_cl_gain.dat')  
 plt.plot(data2[:,0],data2[:,1])  
 plt.grid()
 plt.xlabel("Time")
 plt.ylabel("Gain")
-plt.title('Feedback gain')
+plt.title("Closed loop gain")
 
 # if using termux
-plt.savefig('./figs/ep18btech11016/ep18btech11016_feed_gain.pdf')
-plt.savefig('./figs/ep18btech11016/ep18btech11016_feed_gain.eps')
-subprocess.run(shlex.split("termux-open ./figs/ep18btech11016/ep18btech11016_feed_gain.pdf"))
+#plt.savefig('./figs/ep18btech11016/ep18btech11016_cl_gain.pdf')
+#plt.savefig('./figs/ep18btech11016/ep18btech11016_cl_gain.eps')
+#subprocess.run(shlex.split("termux-open ./figs/ep18btech11016/ep18btech11016_cl_gain.pdf"))
 # else
-#plt.show()
-
-data3=np.loadtxt('ep18btech11016_cl_gain.dat')  
-plt.plot(data3[:,0],data3[:,1])  
-plt.grid()
-plt.xlabel("Time")
-plt.ylabel("Gain")
-plt.title('Closed loop gain')
-
-# if using termux
-plt.savefig('./figs/ep18btech11016/ep18btech11016_cl_gain.pdf')
-plt.savefig('./figs/ep18btech11016/ep18btech11016_cl_gain.eps')
-subprocess.run(shlex.split("termux-open ./figs/ep18btech11016/ep18btech11016_cl_gain.pdf"))
-# else
-#plt.show()
+plt.show()
 
 
